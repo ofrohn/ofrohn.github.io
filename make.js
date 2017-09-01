9ae1c39736769647507c9fad37eb378e019a1ec1
ofrohn <ol.frohn@gmail.com>
2017-09-01 11:45:07 +0200

    stats

--- make.js
+++ make.js
@@ -1,5 +1,8 @@
 var shell = require('shelljs/make'),
-    fs = require('fs');
+    fs = require('fs'),
+    ga = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-105720254-1', 'auto');ga('send', 'pageview');</script>";
+
+
 
 echo('Copying d3-celestial');
   
@@ -79,3 +82,8 @@ cp('-fR', '../seh/images/*', 'images');
 cp('-fR', '../seh/doc/*', 'seh-doc');
 
 echo('Done');
+
+ls('*.html', 'celestial-demo/*.html', 'seh-doc/*.html', 'threex-planets-demo/*.html').forEach(function (file) {
+  if (grep('UA-105720254-1', file).length > 1) return;
+  sed('-i', '</body>', ga + '\n</body>', file);
+});
