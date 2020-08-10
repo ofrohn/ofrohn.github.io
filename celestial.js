4515d092e1f388dd0e2b6581825d3df8a1565f83
ofrohn <ofroh@web.de>
2020-08-10 20:52:08 +0200

    New d3-Celestial

--- celestial.js
+++ celestial.js
@@ -1,7 +1,7 @@
 // Copyright 2015-2020 Olaf Frohn https://github.com/ofrohn, see LICENSE
 !(function() {
 var Celestial = {
-  version: '0.7.28',
+  version: '0.7.29',
   container: null,
   data: []
 };
